e8a6a5a90e181ae1788303959beb9f6453b42b6b
E. S. Adsiz <98318801+esadsiz@users.noreply.github.com>
2022-11-02 06:39:00 +0300

    Create AdminPanel.py

---
+++ AdminPanel.py
@@ -0,0 +1,21 @@
+################################################################################################
+# pip install Faker: veri tabanimiza fake veriler girer. Test amacli kullanilir.
+# Daha sonra python manage.py shell yazarak shell'e gireriz. Ardindan su komutlari gireriz:
+
+# from urunapp.models import UrunModeli
+# from faker import Faker
+# faker = Faker()
+# for i in range(1,200):
+# urun = UrunModeli(urunismi=faker.name(), urunaciklamasi=faker.paragraph(), stok_durumu=False)
+# urun.save()
+
+#####
+
+# pip install django-ckeditor word tarzi bir text editör paketidir.
+################################################################################################
+#
+#
+#
+#
+#
+################################################################################################
